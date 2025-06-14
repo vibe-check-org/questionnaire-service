636d0cb7c9bf661585a9e0953b2cfba178c02570
Caleb-Script <caleb-script@outlook.de>
2025-06-14 23:27:43 +0200

    chore: add CD/CI

--- src/security/keycloak/keycloak.module.ts
+++ src/security/keycloak/keycloak.module.ts
@@ -1,43 +1,30 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
+// eslint-disable-next-line max-classes-per-file
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
-import {
-    AuthGuard,
-    KeycloakConnectModule,
-    // RoleGuard,
-} from 'nest-keycloak-connect';
+import { KeycloakConnectModule } from 'nest-keycloak-connect';
 import { KeycloakService } from './keycloak.service.js';
 import { KeycloakGuard } from './guards/keycloak.guard.js';
 
 @Module({
-    providers: [KeycloakService],
-    exports: [KeycloakService],
+  providers: [KeycloakService],
+  exports: [KeycloakService],
 })
 class ConfigModule {}
 
 @Module({
-    imports: [
-        KeycloakConnectModule.registerAsync({
-            useExisting: KeycloakService,
-            imports: [ConfigModule],
-        }),
-    ],
-    providers: [
-        KeycloakService,
-        {
-            // fuer @UseGuards(AuthGuard)
-            provide: APP_GUARD,
-            useClass: AuthGuard,
-        },
-        {
-            // fuer @Roles({ roles: ['admin'] }) einschl. @Public() und @AllowAnyRole()
-            provide: APP_GUARD,
-            // useClass: RoleGuard,
-            useClass: KeycloakGuard,
-        },
-    ],
-    exports: [KeycloakConnectModule, KeycloakService],
+  imports: [
+    KeycloakConnectModule.registerAsync({
+      useExisting: KeycloakService,
+      imports: [ConfigModule],
+    }),
+  ],
+  providers: [
+    KeycloakService,
+    {
+      provide: APP_GUARD,
+      useClass: KeycloakGuard,
+    },
+  ],
+  exports: [KeycloakConnectModule, KeycloakService],
 })
 export class KeycloakModule {}
